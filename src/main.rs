31879adae825ab4fb53f6efbd9b1491b1a9dd3c2
spagdoon0411 <spandan.suthar@live.com>
2026-08-06 16:08:32 -0700

    Initialize app's user data dir

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use std::fs;
+use std::io::{self, Write};
+use std::path::Path;
+use std::process::ExitCode;
+
+const USER_DATA_DIR: &str = "user_data";
+
+fn verify_user_data() -> io::Result<()> {
+    if Path::new(USER_DATA_DIR).is_dir() {
+        println!("Found local data.")
+    } else {
+        println!("No user data found.");
+
+        print!("Create user_data directory? (y/n): ");
+        io::stdout().flush()?;
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+
+        if input.trim().eq_ignore_ascii_case("y") {
+            fs::create_dir(USER_DATA_DIR)?;
+            println!("Created user_data directory.");
+        } else {
+            println!("Skipped creating user_data directory.");
+        }
+    }
+
+    Ok(())
+}
+
+fn main() -> ExitCode {
+    match verify_user_data() {
+        Ok(()) => {}
+        Err(err) => {
+            eprintln!("error: {err}");
+            return ExitCode::FAILURE;
+        }
+    }
+
+    // TODO: enter primary application
+
+    ExitCode::SUCCESS
+}
